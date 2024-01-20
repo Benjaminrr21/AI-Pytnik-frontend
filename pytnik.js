fa521658a89a7e0affde35c1a4105ab2b4a5ef27
Benjamin <benjaminramovic02@gmail.com>
2024-01-20 23:56:16 +0100

    Second commit

--- pytnik.js
+++ pytnik.js
@@ -146,7 +146,8 @@ else if(agentSel == 'Uki') pretrazivanje.innerHTML = 'BRANCH AND BOUND';
 else if(agentSel == 'Micko') pretrazivanje.innerHTML = 'A*';
 
 //SLANJE ZAHTEVA
-const url = 'http://127.0.0.1:8000/game/path/';
+///const url = 'http://127.0.0.1:8000/game/path/';
+const url = 'https://pytnikbackend.vercel.app/game/path/';
 const data = {
   matrica: matrica,
   agent: agentSel
